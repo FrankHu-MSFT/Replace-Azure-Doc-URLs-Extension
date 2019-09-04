de70bc8951af962af858e3e7f0787273abbd4e6f
Frank Hu <frhu@microsoft.com>
2019-09-03 18:04:13 -0700

    Making some Updates
    Making it run once within half a second when a page is first loaded.
    
    Then afterwards, there is a 1 minute grace period you can still access the github issue page. After accessing it, the injection will work properly otherwise it won't make the change to the html. I think it might be due to the fact I'm using JQuery and jquery might need the page to actually be accessed before running properly.

--- Add -PR/background.js
+++ Add -PR/background.js
@@ -1,12 +1,48 @@
 // listen for our browerAction to be clicked
+
+// https://stackoverflow.com/questions/8490385/chrome-extension-execute-every-x-minutes
 chrome.tabs.onUpdated.addListener(function (tabId , info, tab) {
-	console.log("current status = " + info.status);
-	if(info.status == 'complete'){
-		console.log("running injection");
+	if(tab.url.includes("MicrosoftDocs/azure-docs/issues")){
+		console.log("current status = " + info.status);
+		// We run it once for immediate loads. 
 		setTimeout(function () {
-			chrome.tabs.executeScript(tab.tabId, { file: "jquery.js" }, function() {
-				chrome.tabs.executeScript(tab.tabId, { file: "inject.js" });
-			});
-		}, 500);
+							chrome.tabs.executeScript(tab.tabId, { file: "jquery.js" }, function() {
+							chrome.tabs.executeScript(tab.tabId, { file: "inject.js" });
+						});
+					}, 500);
+		
+		// This is to handle when you don't access the github issue tab immediately.
+		
+		// For some reason it only works when you access the tab. 
+		// The tab doesn't seem to load otherwise? Not sure what's going on. 
+		// The injection definitely just doesn't work.
+		
+		// Setinterval works in Miliseconds. 
+		// so : 
+		// 1000 ms * x = X seconds
+		var timeToRunInMiliseconds = 60000; // 1 minute
+		var injectionTimeInMiliseconds = 1000;
+		
+		if(info.status == 'complete'){
+			console.log("running injection " + timeToRunInMiliseconds/injectionTimeInMiliseconds + " times");
+			
+			// Run every 1 second
+			var myIntervals={};
+			myIntervals[0] = setInterval(function(){
+				setTimeout(function () {
+							chrome.tabs.executeScript(tab.tabId, { file: "jquery.js" }, function() {
+							chrome.tabs.executeScript(tab.tabId, { file: "inject.js" });
+						});
+					}, injectionTimeInMiliseconds);
+			}, injectionTimeInMiliseconds); 
+			
+			// Stops the timer function after 
+			// (timeToRunInMiliseconds/injectionTimeInMiliseconds) seconds. 
+			
+			var timedEvent = setTimeout(function() {
+				console.log("clearing interval");
+				clearInterval(myIntervals[0]);
+			}, timeToRunInMiliseconds);
+		}
 	}
 });

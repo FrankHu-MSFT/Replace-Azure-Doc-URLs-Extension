d2a6fcfc72657e4ae1cfaaa616e75402cda46876
Frank Hu <frhu@microsoft.com>
2019-09-05 15:50:53 -0700

    Adding GA Tracking Code Via Background.js
    Adding GA Tracking Code Via Background.js

--- Add -PR/background.js
+++ Add -PR/background.js
@@ -5,14 +5,39 @@
 // Changelog:
 // 0.4 adding analytics code for metric handling. 
 
+var _AnalyticsCode = 'UA-147217527-1';
+
+/**
+ * Below is a modified version of the Google Analytics asynchronous tracking
+ * code snippet.  It has been modified to pull the HTTPS version of ga.js
+ * instead of the default HTTP version.  It is recommended that you use this
+ * snippet instead of the standard tracking snippet provided when setting up
+ * a Google Analytics account.
+ */
+
+// Standard Google Universal Analytics code
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga'); // Note: https protocol here
+ga('create', _AnalyticsCode, 'auto');
+ga('set', 'checkProtocolTask', function(){}); // Removes failing protocol check. @see: http://stackoverflow.com/a/22152353/1958200
+ga('require', 'displayfeatures');
+
 
 chrome.tabs.onUpdated.addListener(function (tabId , info, tab) {
-	if(tab.url.includes("MicrosoftDocs/azure-docs/issues")){
+	if(tab.url.includes("MicrosoftDocs/azure-docs/issues") && info.status == "complete"){
 		console.log("current status = " + info.status);
 		// We run it once for immediate loads. 
 		setTimeout(function () {
 							chrome.tabs.executeScript(tab.tabId, { file: "jquery.js" }, function() {
-							chrome.tabs.executeScript(tab.tabId, { file: "inject.js" });
+							chrome.tabs.executeScript(tab.tabId, { file: "inject.js" }, function(){
+								   if(chrome.runtime.lastError) {
+										  console.log("sending page view");
+										  console.error(chrome.runtime.lastError);
+										  ga('send', 'pageview', '/action-tracked.html');
+									}
+							});
 						});
 					}, 500);
 		
--- Add -PR/inject.js
+++ Add -PR/inject.js
@@ -1,6 +1,6 @@
 /**
  * Add your Analytics tracking ID here.
- */
+ *
 var _AnalyticsCode = 'UA-147217527-1';
 
 /**
@@ -9,31 +9,48 @@ var _AnalyticsCode = 'UA-147217527-1';
  * instead of the default HTTP version.  It is recommended that you use this
  * snippet instead of the standard tracking snippet provided when setting up
  * a Google Analytics account.
- */
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', _AnalyticsCode]);
-_gaq.push(['_ActionPerformed']);
-(function() {
-  var ga = document.createElement('script');
-  ga.type = 'text/javascript';
-  ga.async = true;
-  ga.src = 'https://ssl.google-analytics.com/ga.js';
-  var s = document.getElementsByTagName('script')[0];
-  s.parentNode.insertBefore(ga, s);
-})();
+ *
 
+// Standard Google Universal Analytics code
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga'); // Note: https protocol here
+ga('create', _AnalyticsCode, 'auto');
+ga('set', 'checkProtocolTask', function(){}); // Removes failing protocol check. @see: http://stackoverflow.com/a/22152353/1958200
+ga('require', 'displayfeatures');
+ga('send', 'pageview', '/action-tracked.html');
+*/
+
+// I cannot figure out how to get this google analytics to work in the Javascript injection, i don't know if it's supposed to work .
+
+// Performing GA tracking in background.js. 
+
+// Possible solution below? 
+// https://stackoverflow.com/questions/23895377/sending-message-from-a-background-script-to-a-content-script-then-to-a-injected/23895822
+
+
+// Helpful information
+// https://www.optimizesmart.com/beginners-guide-to-javascript-for-google-analytics/
 
 (function() {
 	console.log("Running Injection Script Inject.js");   
+	
 	$('a').each(function() { 
 		var $this = $(this),aHref = $this.attr('href');  //get the value of an attribute 'href'
-		
+		var replaced = 0;
 		// Checks if this link is an azure-docs link
 		if($this.attr('href').indexOf("https://github.com/Microsoft/azure-docs/") > -1){
-			// Need to insert google analytics code here.
-			_gaq.push(['_trackEvent', 'Azure-Docs-Pr-Change', 'ActionPerformed', 'AVG time saved 30 seconds and 10 clicks/types each time.']);
+			// Pushing track event to _gaq. Not sure how this is supposed to work yet... 
+			
+			//_gaq.push(['_trackEvent', 'Azure-Docs-Pr-Change', 'ActionPerformed', 'AVG time saved 30 seconds and 10 clicks/types each time.']);
+			// ga.push(['_trackEvent', "1", 'ActionPerformed']);
+			
+			replaced=1;
 			console.log($this.attr('href'));
 			$this.attr('href', aHref.replace('https://github.com/Microsoft/azure-docs/','https://github.com/Microsoft/azure-docs-pr/'));//set the value of an attribute 'href'
+		} else if(Boolean(replaced)){
+			throw Error("Replaced value, sending message to main tab. " + tabId);	
 		}
 	});
 })();
